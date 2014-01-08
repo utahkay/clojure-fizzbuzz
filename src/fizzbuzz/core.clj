43349dc7bcd49a363ffdf83a152976bc976d5833
kay-johansen <kay-johansen@pluralsight.com>
2014-01-07 23:18:07 -0700

    Working implementation

---
+++ src/fizzbuzz/core.clj
@@ -0,0 +1,13 @@
+(ns fizzbuzz.core)
+
+(defn fizz-buzz [x]
+  (cond
+    (zero? (mod x 15)) "FizzBuzz"
+    (zero? (mod x 3)) "Fizz"
+    (zero? (mod x 5)) "Buzz"
+    :else (str x)))
+
+(defn fizz-buzz-seq [from to]
+  (map fizz-buzz (range from (inc to))))
+
+
---
+++ test/fizzbuzz/core_test.clj
@@ -0,0 +1,27 @@
+(ns fizzbuzz.core-test
+  (:require [clojure.test :refer :all]
+            [fizzbuzz.core :refer :all]))
+
+(deftest one-should-be-one
+  (is (= "1" (fizz-buzz 1))))
+
+(deftest two-should-be-two
+  (is (= "2" (fizz-buzz 2))))
+
+(deftest three-should-fizz
+  (is (= "Fizz" (fizz-buzz 3))))
+
+(deftest five-should-buzz
+  (is (= "Buzz" (fizz-buzz 5))))
+
+(deftest six-should-fizz
+  (is (= "Fizz" (fizz-buzz 6))))
+
+(deftest fifteen-should-fizz-buzz
+  (is (= "FizzBuzz" (fizz-buzz 15))))
+
+(deftest sequence-fizz-buzz
+  (is (= '("1" "2" "Fizz" "4" "Buzz") (fizz-buzz-seq 1 5))))
+
+
+
